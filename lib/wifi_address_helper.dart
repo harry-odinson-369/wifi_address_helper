574e911985132d76ab65999dff195ec9e72ebe1b
Harry Odinson <vandet.dev@gmail.com>
2025-08-05 15:26:04 +0700

    up

--- lib/wifi_address_helper.dart
+++ lib/wifi_address_helper.dart
@@ -1,9 +1,6 @@
-
 import 'wifi_address_helper_platform_interface.dart';
 
 class WifiAddressHelper {
   static Future<String?> getPlatformVersion() => WifiAddressHelperPlatform.instance.getPlatformVersion();
-
   static Future<String?> get getAddress => WifiAddressHelperPlatform.instance.getAddress;
-
 }
